354d054e5c9894823fafd1fd31d85398eb17ee5c
klc <nutttty@gmail.com>
2015-02-07 14:52:12 -0800

    initial stuff

---
+++ hippo/hippo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hippo
+//
+//  Created by Kelsey on 2/7/15.
+//  Copyright (c) 2015 Kelsey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
